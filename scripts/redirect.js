af847c216b90b5e1fcb8de6f4ece38290c2e7055
Thomas Szatmary <u7303252@anu.edu.au>
2023-10-16 21:55:51 +1100

    added pages

---
+++ scripts/redirect.js
@@ -0,0 +1,5 @@
+if(localStorage.getItem("loggedin") == null || localStorage.getItem("loggedin") == false){
+    window.location.href = "./login/signin/"
+}
+
+// localStorage.setItem("lastname", "Smith")
