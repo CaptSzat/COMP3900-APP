c95b7983ba2faa8048149a405dcd1fe5ea963d22
Thomas Szatmary <u7303252@anu.edu.au>
2023-10-24 10:39:28 +1100

    added guide

---
+++ scripts/guide.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    setTimeout(() => {
+        alert("Green dots mean that you are going to an event on that date. Orange dots mean that a club you are registered with is holding an event. Blue dots mean that an event is occuring that a friend of yours is attending.");
+    }, 7000);
+}
+
+myFunction();
